7a4544a487a08268936897239368f19988cb2a3f
Sergio Rivas <s.rivasd@alumnos.urjc.es>
2020-03-22 18:59:27 +0100

    Requirements updated

---
+++ requirements.sh
@@ -0,0 +1,13 @@
+sudo apt-get -y install python python-pip python-dev libffi-dev libssl-dev
+sudo apt-get -y install python-virtualenv python-setuptools
+sudo apt-get -y install libjpeg-dev zlib1g-dev swig
+sudo apt-get -y install mongodb
+sudo apt-get -y install postgresql libpq-dev
+sudo apt-get -y install libfuzzy-dev
+sudo apt-get -y install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
+sudo pip install XenAPI
+sudo apt-get -y install tcpdump apparmor-utils
+sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+sudo apt-get -y install volatility
+sudo pip install m2crypto
+sudo apt-get -y install openjdk-8-jre libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386
